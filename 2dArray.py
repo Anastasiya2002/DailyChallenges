236f6ffdfe573de27c9054bcf318392ef999646f
Anastasiya2002 <56453860+Anastasiya2002@users.noreply.github.com>
2020-03-29 05:06:59 -0400

    Add files via upload
    Fixed version so it would work with negative numbers.

--- 2dArray.py
+++ 2dArray.py
@@ -1,17 +1,17 @@
 #define the largest hourglass
-arr= [[1,1,1,0,0,0],
-     [1,1,0,0,0,0],
-     [1,1,1,0,0,0],
-     [0,0,0,0,0,0],
-     [0,0,0,0,0,0],
-     [0,0,0,0,0,0]]
+arr= [[-1,-1,0,-9,-2,-2],
+     [-2,-1,-6,-8,-2,-5],
+     [-1,-1,-1,-2,-3,-4],
+     [-1,-9,-2,-4,-4,-5],
+     [-7,-3,-3,-2,-9,-9],
+     [-1,-3,-1,-2,-4,-5]]
 
 def hourglassSum(arr):
    rows = len(arr[0])
    col = len(arr) 
    sum = 0
    j = 0
-   maxsum = 0
+   maxsum = arr[0][0] + arr[0][1] + arr[0][2] + arr[1][1] + arr[2][0] + arr[2][1] + arr[2][2]
    for i in range(col - 2):
     for j in range(rows - 2):
         for moveLeft in range(3):
