197ea17552cf375544c136c951a9084ad369a653
AnastasiyaYutsevych <56453860+AnastasiyaYutsevych@users.noreply.github.com>
2020-06-18 06:33:03 -0400

    Add files via upload

--- BestTimetoBuyandSellStock.py
+++ BestTimetoBuyandSellStock.py
@@ -1,19 +1,9 @@
-def maxProfit(prices):
-    profit = 0
-    while len(prices)!= 0 and len(prices)!= 1:
-       maximum = max(prices)
-       minimum = min(prices)
-       if prices.index(minimum) < prices.index(maximum):
-           arr = prices[prices.index(minimum)+1:prices.index(maximum)]
-           if prices.index(max(arr)) > prices.index(minimum) and  prices.index(min(arr)) < prices.index(maximum) and prices.index(min(arr)) >  prices.index(max(arr)):
-               if maximum - minimum < max(arr) - minimum + maximum - min(arr):
-                   profit += max(arr) - minimum + maximum - min(arr)  
-               else:
-                   profit += maximum - minimum
-           else:
-                profit += maximum - minimum
-           prices = prices[:prices.index(minimum)]+prices[prices.index(maximum)+1:]
-       else:    
-           prices.remove(maximum)
-    return profit
-print(maxProfit([6,1,3,2,4,7]))
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not len(prices): return 0
+        dp = [[0,0] for _ in range(len(prices))]
+        dp[0][1] = -prices[0]
+        for i in range(1,len(prices)):
+            dp[i][0] = max(dp[i-1][0],dp[i-1][1]+prices[i])
+            dp[i][1] = max(dp[i-1][1],-prices[i])
+        return dp[-1][0]
