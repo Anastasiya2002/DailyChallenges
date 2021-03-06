1614b5b4a4f7fbb95b04048de33060788890fe6a
AnastasiyaYutsevych <56453860+AnastasiyaYutsevych@users.noreply.github.com>
2021-03-06 13:39:48 +0200

    Add files via upload

--- BestTimetoBuyandSellStock.py
+++ BestTimetoBuyandSellStock.py
@@ -1,9 +1,24 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        if not len(prices): return 0
-        dp = [[0,0] for _ in range(len(prices))]
-        dp[0][1] = -prices[0]
-        for i in range(1,len(prices)):
-            dp[i][0] = max(dp[i-1][0],dp[i-1][1]+prices[i])
-            dp[i][1] = max(dp[i-1][1],-prices[i])
-        return dp[-1][0]
+#You are given an array prices for which the ith element is the price of a given stock on day i.
+#Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+#Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+def maxProfit(prices):
+    if len(prices) == 1 or len(prices)==0:
+        return 0
+    x = min(prices)
+    y = max(prices)
+    ind_1= prices.index(x)
+    ind_2 = prices.index(y)
+    if ind_1 < ind_2 and len(prices[ind_1:ind_2])!=0:
+        return max((y-x)+maxProfit(prices[:ind_1])+maxProfit(prices[ind_2+1:]),maxProfit(prices[ind_1:ind_2])+maxProfit(prices[prices.index(max(prices[ind_1:ind_2]),ind_1,ind_2)+1:ind_2+1]))
+    elif ind_1<ind_2 and len(prices[ind_1:ind_2])==0:
+        return (y-x)+maxProfit(prices[:ind_1])+maxProfit(prices[ind_2+1:])
+    elif len(prices[ind_1:ind_2])!=0:
+        return  maxProfit(prices[:ind_1])+maxProfit(prices[ind_2+1:])+maxProfit(prices[ind_1+1:ind_2+1])+maxProfit(prices[max(prices[ind_1:ind_2])+1:ind_2+1])
+    else:
+        return  maxProfit(prices[:ind_1])+maxProfit(prices[ind_2+1:])
+
+
+print(maxProfit([5,4,3,2,1])) #-> returns 0
+print(maxProfit([7,1,5,3,6,4])) #-> return 7
+print(maxProfit([1,2,3,4,5])) #->return 4
+
